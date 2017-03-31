eb60943388d55ed6861c8b2a34a8e4504f5d1c43
Alessandro Aglietti <alessandro.aglietti@gmail.com>
2017-03-31 17:07:09 +0200

    31-03-2017 - screenshoot howto cdnjs - file per paragonare prima e dopo jquery - handler onclick dichiarato separato

---
+++ 31-03-2017/2_jquery_calcolatrice/calc_vanilla.js
@@ -0,0 +1,66 @@
+function leggiAddendi() {
+  var addendi   = [];
+  var addendo_1 = document.getElementById( "addendo1" ).value;
+  var addendo_2 = document.getElementById( "addendo2" ).value;
+  console.log( "addendo1: " + addendo_1 );
+  console.log( "addendo2: " + addendo_2 );
+
+  addendi[ 0 ] = parseFloat( addendo_1 );
+  addendi[ 1 ] = parseFloat( addendo_2 );
+
+  return addendi;
+}
+
+function esegui_somma() {
+  var addendi   = leggiAddendi(); // array
+  var risultato = addendi[ 0 ] + addendi[ 1 ];
+  if ( isNaN( risultato ) ) {
+    // se è NaN
+    alert( "OMG! ERRORE!" );
+  } else {
+    // altrimenti
+    visualizza_operazione( addendi[ 0 ], addendi[ 1 ], "+", risultato );
+  }
+}
+
+function esegui_sottrazione() {
+  var addendi   = leggiAddendi(); // array
+  var risultato = addendi[ 0 ] - addendi[ 1 ];
+  if ( isNaN( risultato ) ) {
+    // se è NaN
+    alert( "OMG! ERRORE!" );
+  } else {
+    // altrimenti
+    visualizza_operazione( addendi[ 0 ], addendi[ 1 ], "-", risultato );
+  }
+}
+
+function esegui_divisione() {
+  var addendi   = leggiAddendi(); // array
+  var risultato = addendi[ 0 ] / addendi[ 1 ];
+  if ( isNaN( risultato ) ) {
+    // se è NaN
+    alert( "OMG! ERRORE!" );
+  } else {
+    // altrimenti
+    visualizza_operazione( addendi[ 0 ], addendi[ 1 ], "/", risultato );
+  }
+}
+
+function esegui_moltiplicazione() {
+  var addendi   = leggiAddendi(); // array
+  var risultato = addendi[ 0 ] * addendi[ 1 ];
+  if ( isNaN( risultato ) ) {
+    // se è NaN
+    alert( "OMG! ERRORE!" );
+  } else {
+    // altrimenti
+    visualizza_operazione( addendi[ 0 ], addendi[ 1 ], "*", risultato );
+  }
+}
+
+function visualizza_operazione( addendo1, addendo2, operatore, risultato ) {
+  var operazione_testuale                                    = addendo1 + " " + operatore + " " + addendo2;
+  operazione_testuale                                        = operazione_testuale + " = " + risultato;
+  document.getElementById( "operazione_testuale" ).innerText = operazione_testuale;
+}
